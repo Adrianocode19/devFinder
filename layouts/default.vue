8601f08de7f99eec624d03bb997a05c702c96e06
Adriano Carlos Texeira <123494737+Adrianocode19@users.noreply.github.com>
2025-05-10 11:29:33 -0400

    Add files via upload
    feat: subindo aplicação para o GitHub

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <main
+    class="flex flex-col px-6 py-8 bg-secondary min-h-screen max-h-full w-full"
+  >
+    <section
+      class="flex flex-col justify-center w-full max-w-[327px] md:max-w-[573px] xl:max-w-[730px] xl:min-w-[730px] mx-auto px-4"
+    >
+      <slot />
+    </section>
+  </main>
+</template>
